331bc4ca771090eeebea2e48705bd5ab32e6d41d
RobertZSun <robertsunzhe@hotmail.com>
2019-09-24 20:54:37 -0400

    fix the bug of negative false digit

--- Week 3 MillerRabin/assignmentThree.java
+++ Week 3 MillerRabin/assignmentThree.java
@@ -1,3 +1,4 @@
+
 /*
  * @Description:
  * Use the Miller Rabin algorithm together with power mod
@@ -24,43 +25,54 @@ public class assignmentThree {
         return product;
     }
 
-    public static boolean miller_rabin(int n) {
+    public static boolean miller_rabin(int n) throws Exception {
         Random random = new Random();
-        if (n == 2) {
+        if (n <= 0) {
+            throw new Exception("the number must be greater than ZERO!");
+        }
+        if(n==2){
             return true;
         }
-        if (n % 2 == 0 || n == 1) {
+        if(n%2==0||n==1){
             return false;
         }
 
-        int frontNum = n - 1, exponentOfTwo = 0;
-        while (frontNum % 2 == 0) {
+        int frontNum = n - 1, exponentOfTwo = 0;while(frontNum%2==0)
+        {
             frontNum /= 2;
             exponentOfTwo++;
         }
-        int loopTime = 6;
-        out: for (int i = 0; i < loopTime; i++) {
-            int randomNum_a = random.nextInt(n - 2) + 2;
-            long x0 = powerMod(randomNum_a, frontNum, n);
-            if (x0 == 1 || x0 == n - 1) {
-                continue;
-            }
-            for (int j = 0; j < exponentOfTwo - 1; j++) {
-                x0 = (x0 * x0) % n;
-                if (x0 == 1) {
-                    return false;
-                }
-                if (x0 == n - 1) {
-                    continue out;
-                }
-            }
-            return false;
+        int loopTime = 6;out:for(
+        int i = 0;i<loopTime;i++)
+        {
+        int randomNum_a = random.nextInt(n - 2) + 2;
+        long x0 = powerMod(randomNum_a, frontNum, n);
+        if (x0 == 1 || x0 == n - 1) {
+            continue;
         }
-        return true;
+        for (int j = 0; j < exponentOfTwo - 1; j++) {
+            x0 = (x0 * x0) % n;
+            if (x0 == 1) {
+                return false;
+            }
+            if (x0 == n - 1) {
+                continue out;
+            }
+        }
+        return false;
+    }return true;
     }
 
     public static void main(String[] args) {
-        if (miller_rabin(Integer.parseInt(args[0]))) {
+        boolean result = false;
+        try {
+            result = miller_rabin(Integer.parseInt(args[0]));
+        } catch (Exception e) {
+            System.out.println("Got an exception: "+ e.getMessage());
+            e.printStackTrace();;
+        }
+
+        if (result) {
             System.out.println("this is probably a prime number");
         } else {
             System.out.println("this is not a prime number");
